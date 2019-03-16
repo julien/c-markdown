a0a96e1462167417c3fda5b1a7deb013ccf5e71f
Julien Castelain <julien.castelain@liferay.com>
2019-03-16 19:57:11 +0100

    Formatting

--- stack.c
+++ stack.c
@@ -5,8 +5,7 @@ char stack[1000] = {'\0'};
 int size()
 {
     int i = 0;
-    while(stack[i] != '\0')
-    {
+    while (stack[i] != '\0') {
         i++;
     }
     return i;
@@ -21,8 +20,8 @@ char top()
 {
     int i = size();
 
-    if(empty()) return -1;
-    else return stack[i-1];
+    if (empty()) return -1;
+    else return stack[i - 1];
 }
 
 void push(char c)
@@ -30,12 +29,12 @@ void push(char c)
     int i = size(); // i = pierwszy element, ktory nie jest w tablicy
 
     stack[i] = c;
-    stack[i+1] = '\0';
+    stack[i + 1] = '\0';
 }
 
 void pop()
 {
     int i = size();
 
-    stack[i-1] = '\0';
+    stack[i - 1] = '\0';
 }
